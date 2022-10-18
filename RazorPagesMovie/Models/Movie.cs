011114b61541b69df47c086c5c8d9209daeef9ab
allan <marinaallan92@gmail.com>
2022-10-18 14:30:59 -0700

    Created first data model, i.e., Movie Class.

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        //The ID field is required by the database for the primary key
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+
+    }
+}
