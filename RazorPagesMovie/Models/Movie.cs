50d62aab84c91c0a66cb7240380d44a96dbbb2d7
allan <marinaallan92@gmail.com>
2022-10-19 19:33:47 -0700

    Completed part 5 of the tutorial

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,17 +1,32 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
     public class Movie
     {
-        //The ID field is required by the database for the primary key
         public int ID { get; set; }
         public string Title { get; set; } = string.Empty;
 
+        /*
+         * The [Display] attribute specifies the display name of a field. 
+         * In the preceding code, "Release Date" instead of "ReleaseDate".
+         */
+        [Display(Name = "Release Date")]
+        /*
+         * The [DataType] attribute specifies the type of the data (Date). 
+         * The time information stored in the field isn't displayed.
+         */
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; } = string.Empty;
+        /*
+         * The [Column(TypeName = "decimal(18, 2)")] data annotation enables 
+         * Entity Framework Core to correctly map Price to currency in the 
+         * database
+         */
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
-
     }
 }
